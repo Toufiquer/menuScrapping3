4787e63065154828d598d257baf5a98617b49fa4
Toufiquer <toufiquerabc@gmail.com>
2024-11-04 02:53:13 +0600

    done @ open web and scrapping all heading and save to menu.json file

--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@
 - [foodhub](https://foodhub.co.uk/hillhead-pkwy/raj-bari-indian-takeawayrestaurant/ordernow)
 - [fattoush](https://fattoushne4.co.uk/takeaways/Newcastle/NE4/shawarma-fattoush/menu)
 - [just-eat](https://www.just-eat.co.uk/restaurants-shawarmafattoush-ne4/menu)
+
+
+
+Step 1: git clone https://github.com/Toufiquer/menuScrapping3.git
+step 2: yarn
+step 3: yarn dev [for development watch index.ts and write to dist/index.js]
+step 4: yarn start [for scrapping the project]
--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,70 @@ import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
 import fs = require("fs");
+import puppeteer = require("puppeteer");
 
 const rl = readline.createInterface({ input, output });
 
+// ! Start project
 const run = async () => {
-  let menuData = [];
+  let foodHubMenuData = [];
+
+  //   ! get url
+  const foodHubURL = "https://foodhub.co.uk/hillhead-pkwy/raj-bari-indian-takeawayrestaurant/ordernow";
+
+  //   ! Open browser
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.setViewport({
+    width: 1920, // Full-screen width
+    height: 1080, // Full-screen height
+    deviceScaleFactor: 1,
+    hasTouch: false,
+    isMobile: false,
+  });
+
+  await page.goto(foodHubURL, { waitUntil: "networkidle2", timeout: 60000 });
+  console.log("Please wait... it takes about 5-6 minutes");
+
+  // ! 1. take a snapshot of the web
+  const primeMenuData = await page.evaluate(async () => {
+    const smoothScrollToBottom = async () => {
+      let scrollPosition = 0;
+      let documentHeight = document.body.scrollHeight;
+      while (documentHeight > scrollPosition) {
+        const targetPosition = Math.min(documentHeight, scrollPosition + 1000);
+
+        await new Promise((resolve) => {
+          window.scrollTo({
+            top: targetPosition,
+            behavior: "smooth",
+          });
+          setTimeout(resolve, 100);
+        });
+
+        scrollPosition = targetPosition;
+        documentHeight = document.body.scrollHeight;
+      }
+    };
+    await smoothScrollToBottom();
+
+    await new Promise((resolve) => setTimeout(resolve, 200));
+
+    const div = document.querySelectorAll("h2[role='heading']");
+    console.log("div : ", div);
+    const innerText = [];
+    for (const child of div) {
+      innerText.push(child.textContent.trim());
+      console.log(child.textContent.trim());
+    }
+
+    return innerText;
+  });
+  foodHubMenuData.push(...primeMenuData);
 
   //   ! Save data to menu.json
-  fs.writeFile("menu.json", JSON.stringify(menuData), (err) => {
+  fs.writeFile("menu.json", JSON.stringify(foodHubMenuData), (err) => {
     if (err) {
       console.error(err);
       return;
@@ -26,6 +82,8 @@ const run = async () => {
   });
 
   //   ! Close window
+  await browser.close();
   rl.close();
 };
+
 run();
