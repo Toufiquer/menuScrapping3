23ab77a928cccabf2466f85e0025634cca95305c
Toufiquer <toufiquerabc@gmail.com>
2024-11-05 04:44:41 +0600

    incomplete - scrapping is not complete, details in project-update.ts

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,6 @@ const rl = readline.createInterface({ input, output });
 const run = async () => {
   let foodHubMenuData = [];
 
-  //   ! get url
   const foodHubURL = "https://foodhub.co.uk/hillhead-pkwy/raj-bari-indian-takeawayrestaurant/ordernow";
 
   //   ! Open browser
@@ -36,63 +35,166 @@ const run = async () => {
   await page.goto(foodHubURL, { waitUntil: "networkidle2", timeout: 60000 });
   console.log("Please wait... it takes about 5-6 minutes");
 
-  // ! 1. take a snapshot of the web
-  const primeMenuData = await page.evaluate(async () => {
-    // ! smoothScroll to load all data
-    const smoothScrollToBottom = async () => {
-      let scrollPosition = 0;
-      let documentHeight = document.body.scrollHeight;
-      while (documentHeight > scrollPosition) {
-        const targetPosition = Math.min(documentHeight, scrollPosition + 1000);
-
-        await new Promise((resolve) => {
-          window.scrollTo({
-            top: targetPosition,
-            behavior: "smooth",
-          });
-          setTimeout(resolve, 100);
-        });
-
-        scrollPosition = targetPosition;
-        documentHeight = document.body.scrollHeight;
-      }
-    };
-    await smoothScrollToBottom();
-
-    await new Promise((resolve) => setTimeout(resolve, 200));
-
-    // get all header
-    const div = document.querySelectorAll("div[id='category-section']");
-
-    console.log("div : ", div);
-    const innerText = [];
-    for (const child of div) {
-      innerText.push(child.textContent.trim());
-      console.log(child.textContent.trim());
-    }
-
-    const div2 = document.querySelectorAll("div[id='menu-list-section']");
-    console.log("div2 : ", div2);
-    const innerText2 = [];
-    for (const child2 of div2) {
-      innerText.push(child2.textContent.trim());
-      console.log(child2.textContent.trim());
-    }
-
-    return innerText;
-  });
-  foodHubMenuData.push(...primeMenuData);
-
   // ! Get all Heading and clicking them one by one
   const getAllElement = await page.$$("div[id='category-section'] h2[role='heading']");
+  const parentPrimeMenuData = [];
   for (const element of getAllElement) {
     element.click();
-    console.log("element clicked ", await (await element.getProperty("textContent")).jsonValue());
+    const t = await (await element.getProperty("textContent")).jsonValue();
+    console.log("element clicked ", t);
+
+    // ! after click the element then scrap all data and then check it already exists or not if not then update it.
+    const scrappingAllData = await page.evaluate(async () => {
+      const div = document.querySelectorAll("div[data-test-id='virtuoso-item-list']");
+
+      const getNodeElements = (arrOfNodeElement) => {
+        let filterElementNodes = [];
+        for (const child of arrOfNodeElement) {
+          if (child.nodeType !== 8) {
+            // This filters out comments (nodeType 8)
+            const filterChild = {
+              childElementCount: "",
+              children: "",
+              innerText: "",
+              tagName: "",
+              textContent: "",
+              classList: "",
+            };
+            for (const c in child) {
+              if (child[c] !== null) {
+                filterChild[c] = child[c];
+              }
+            }
+            const newChild = {
+              childElementCount: filterChild.childElementCount,
+              children: filterChild.children,
+              innerText: filterChild.innerText,
+              tagName: filterChild?.tagName,
+              textContent: filterChild.textContent,
+              // selfNode: filterChild,
+              classList: filterChild.classList,
+            };
+            filterElementNodes.push({ ...newChild });
+          }
+        }
+        //   clear child nodes by self invoked
+        filterElementNodes = filterElementNodes.map((curr) => {
+          if (curr.childElementCount > 0) {
+            const filterChildren = getNodeElements(curr.children);
+            return {
+              ...curr,
+              children: filterChildren,
+            };
+          } else {
+            return curr;
+          }
+        });
+        return filterElementNodes
+          .filter((curr) => curr.innerText !== "")
+          .filter((curr) => curr.innerText !== undefined);
+      };
+
+      const nodeElementFromDiv = getNodeElements(div);
+      console.log(nodeElementFromDiv);
+
+      const getDataByTagName = (data, tagName = "") => {
+        let arrData = Array.isArray(data) ? data : [data];
+        if (tagName === "") {
+          return arrData;
+        }
+        const result = [];
+        arrData.forEach((curr) => {
+          if (curr?.tagName.toLowerCase() === tagName.toLowerCase()) {
+            result.push(curr);
+          } else {
+            if (curr?.children?.length > 0) {
+              const findInnerData = getDataByTagName([...curr.children], tagName);
+              result.push(...findInnerData);
+            }
+          }
+        });
+        const filterData = (data) => {
+          const filteredData = data.filter((item, index, self) => {
+            // Check if item is unique
+            return (
+              index ===
+              self.findIndex(
+                (t) =>
+                  t?.tagName === item?.tagName &&
+                  t.children?.length === item.children?.length &&
+                  t.innerText === item.innerText
+              )
+            );
+          });
+          return filteredData;
+        };
+
+        const filterResult = filterData(result);
+        return filterResult;
+      };
+
+      const getInnerText = (obj = { innerText: "", children: [] }, nthChild = 0) => {
+        if (obj.innerText?.includes("\n")) {
+          if (obj.children?.length > 0) {
+            return getInnerText(obj.children[nthChild]);
+          } else {
+            return obj.innerText;
+          }
+        }
+        return obj.innerText;
+      };
+
+      let filteredData = nodeElementFromDiv.map((curr) => {
+        // ! it is confirm that all data has been exist inside curr.
+        // ! now you have to filter it out.
+        const testingData = getInnerText(
+          curr?.children[0]?.children[0]?.children[0]?.children[0]?.children[0]?.children[0]?.children[0]
+        );
+        const item = getInnerText(curr, 0);
+        // const info = getInnerText(curr?.children[0]?.children[1]);
+        // const price = getInnerText(getDataByTagName(curr, "h4")[0])?.split("£")[1];
+        let result: {
+          item: string;
+          price?: string;
+          info?: string;
+          option?: any[];
+          testingData?: string;
+          allInnerText?: string;
+        } = {
+          item,
+          testingData,
+          allInnerText: curr.innerText,
+        };
+        // if (price && price !== "") {
+        //   result.price = price;
+        // }
+        // if (info && !info.includes("£")) {
+        //   result.info = info;
+        // }
+        return result;
+      });
+
+      console.log("");
+      console.log("");
+      console.log("");
+      console.log("filteredData : ", filteredData);
+      return filteredData;
+    });
+
+    parentPrimeMenuData.push({
+      clickedItem: t,
+      isInclude: scrappingAllData[0].allInnerText?.toLowerCase()?.includes(t.toLowerCase()),
+      data: scrappingAllData[0],
+    });
+
     await new Promise((resolve) => setTimeout(resolve, 200));
   }
-
+  console.log("");
+  console.log("");
+  console.log("");
+  // console.log("primeMenuData : ", parentPrimeMenuData);
   //   ! Save data to menu.json
-  fs.writeFile("menu.json", JSON.stringify(foodHubMenuData), (err) => {
+  fs.writeFile("menu.json", JSON.stringify(parentPrimeMenuData), (err) => {
     if (err) {
       console.error(err);
       return;
---
+++ src/project-update.ts
@@ -0,0 +1,3 @@
+// ### from line 148 you have to do this
+// ! it is confirm that all data has been exist inside curr.
+// ! now you have to filter it out.
